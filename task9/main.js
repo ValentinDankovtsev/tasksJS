7ceb64a12474a45ac86ac798076d5e7ddf52addb
wsgilyazetdinov <wsgilyazetdinov@websailors.pro>
2019-06-25 12:39:43 +0300

    first commit

---
+++ task9/main.js
@@ -0,0 +1,139 @@
+;
+(function () {
+    let w = window,
+        d = document;
+
+    const calculator = {
+        flag: false,
+        init: function () {
+
+            d.addEventListener('click', delegateFunction);
+           
+
+        },
+        cash: "",
+        res: 0,
+        //area: d.querySelector('.form-control'),
+        getCash: function () {
+            
+            d.querySelector('.form-control').value = this.cash;
+        },
+        getRes: function () {
+
+            this.res = eval(this.cash);
+            if (!this.res) this.res = 0;
+            d.querySelector('#result').value = this.res;
+        },
+        operation: function (oper) {
+            debugger;
+            if (typeof this.cash.substr(-1) != "number") {
+                this.flag = true;
+            }
+            switch (oper) {
+                case '+':
+                    if (this.flag) {
+                        this.getRes();
+                    }
+                    this.flag = true;
+                    this.cash += oper;
+                    this.getCash();
+                    break;
+                case '-':
+                    if (this.flag) {
+                        this.getRes();
+                    }
+                    this.flag = true;
+
+                    this.cash += oper;
+                    this.getCash();
+                    break;
+                case '=':
+                    this.getRes();
+                    break;
+                case '*':
+                    if (this.flag) {
+                        this.getRes();
+                    }
+                    this.flag = true;
+                    this.cash += oper;
+                    this.getCash();
+                    break;
+                case '/':
+                    if (this.flag) {
+                        this.getRes();
+                    }
+                    this.flag = true;
+                    this.cash += oper;
+                    this.getCash();
+                    break;
+                case '√':
+                    this.getRes();
+                    if (this.res >= 0) {
+                        this.flag = true;
+                        this.cash = Math.sqrt(this.res) + "+";
+                        this.getCash();
+                        //this.getCash();
+                    } else {
+                        this.res = "Error!!!";
+                        this.getRes();
+                        this.getCash();
+                    }
+
+
+                    break;
+                case '.':
+                    if (oper === '' || oper === '0') {
+                        this.doDigit(0);
+                    }
+
+                    this.doDigit(oper);
+                    break;
+                case 'C':
+                    this.cash = " ";
+                    this.res = 0;
+                    this.getRes();
+                    this.getCash();
+                    break;
+                case '%':
+                    if (this.flag) {
+                        let val;
+                        
+                        val =   this.res*this.cash.substr(-1)/100 ;
+                       
+                       this.cash = this.cash.slice(0, -1) + val;
+                       this.flag = true;
+                       this.getRes();
+                       this.getCash();
+                    } else { this.res = "Error!!!";
+                    this.getRes();
+                    this.getCash(); }
+                                        
+                    break;
+
+
+            }
+        },
+        doDigit: function (num) {
+            debugger;
+            this.cash += num;
+            this.getCash();
+        }
+
+    };
+
+
+
+    const delegateFunction = (e) => {
+        let target = e.target;
+
+        if (target.hasAttribute('data-num')) {
+            calculator.doDigit(target.innerText);
+        } else if (target.hasAttribute('data-oper')) {
+            calculator.operation(target.innerText);
+        }
+
+    };
+
+    w.addEventListener('load', calculator.init);
+
+})();
